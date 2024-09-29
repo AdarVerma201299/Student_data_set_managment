e164400fdba73918c15091d13f9e54df416d3bfa
AdarVerma201299 <144684521+AdarVerma201299@users.noreply.github.com>
2024-09-29 09:22:06 +0530

    Update Admin.js
    check login

--- Server/Controller/Admin.js
+++ Server/Controller/Admin.js
@@ -103,6 +103,7 @@ const hanldeAdminLogin = async (req, res) => {
 };
 const hanldeAdminSignUp = async (req, res) => {
   const errors = validationResult(req.body.email);
+  alert("aa gaya mai ");
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array(), success: false });
   }
